05459076a9f2cd617afa4cef2b13b78496387096
Abhay Jeet Zutshi <abhay.03-cse-16@mietjammu.in>
2017-09-22 21:23:10 +0530

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
+// Author :- Abhay Jeet Zutshi
+#include<iostream> 
 using namespace std;
+int i=2;
 int main()
+{  // Scope starts
+int *p; // Pointer used
 {
-int a=1,*p;
-{int a=2;
-p=&a;
-cout<<a;
+int i=-1;
+p=&i;
+} // scope ends
+cout<<*p<<&p<<endl; // Displays the value of variable inside the loop and its address
+cout<<i<<&i<<endl;;
+return 0; // Return the value
 }
-cout<<*p;
-return 0;
-}
+
